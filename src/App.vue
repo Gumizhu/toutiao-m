80b3daa7abc9369d6db6bdb1f96e25529f3d01fc
Gumizhu <gumizhu666@163.com>
2023-03-22 12:27:05 +0800

    项目初始化-调整初始项目结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
+<!--
+ * @Author: Gumizhu
+ * @Date: 2023-03-22 11:42:21
+ * @Descripttion:
+ * @LastEditors: Gumizhu
+ * @LastEditTime: 2023-03-22 12:14:16
+-->
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <h1>黑马头条</h1>
+    <!-- 路由的出口（路由挂载点） -->
+    <router-view />
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App'
 }
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="less"></style>
