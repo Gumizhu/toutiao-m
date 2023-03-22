2e8e3f25947d7fa53f9e38682372d88ea163e1d6
Gumizhu <gumizhu666@163.com>
2023-03-22 19:47:36 +0800

    项目初始化 - 引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
+/*
+ * @Author: Gumizhu
+ * @Date: 2023-03-22 11:42:21
+ * @Descripttion:
+ * @LastEditors: Gumizhu
+ * @LastEditTime: 2023-03-22 19:42:09
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
 // 引入全局样式表
 import './styles/index.less'
+
+// 注册使用 Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
